89f8d2743c7f9281856c29f36e1306b34e2762d2
kwchan19 <37305130+kwchan19@users.noreply.github.com>
2022-05-09 19:38:03 -0400

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+      <wallet-provider :wallets="wallets" auto-connect style="display:none;">
+        <div>
+          <div class="wallet-button">
+            <wallet-modal-provider>
+              <wallet-multi-button></wallet-multi-button>
+            </wallet-modal-provider>
+          </div>
+        </div>
+      </wallet-provider>
+</template>
+
+<script>
+  import {
+    getPhantomWallet,
+    getBitpieWallet,
+    getBloctoWallet,
+    getCoin98Wallet,
+    getLedgerWallet,
+    getMathWallet,
+    getSlopeWallet,
+    getSolflareWallet,
+    getSolflareWebWallet,
+    getSolletWallet,
+    getSolletExtensionWallet,
+    getSolongWallet,
+    getTorusWallet,
+  } from '@solana/wallet-adapter-wallets';
+  import {
+    WalletProvider
+  } from '@solana/wallet-adapter-vue';
+  import {
+    WalletMultiButton,
+    WalletModalProvider
+  } from '@solana/wallet-adapter-vue-ui';
+  const wallets = [
+      getPhantomWallet(),
+      getBitpieWallet(),
+      getBloctoWallet(),
+      getCoin98Wallet(),
+      getLedgerWallet(),
+      getMathWallet(),
+      getMathWallet(),
+      getSlopeWallet(),
+      getSolflareWallet(),
+      getSolflareWebWallet(),
+      getSolletWallet(),
+      getSolletExtensionWallet(),
+      getSolongWallet(),
+      getTorusWallet(),
+  ]
+  import Axios from 'axios';
+  export default {
+    components: {
+      WalletProvider,
+      WalletMultiButton,
+      WalletModalProvider
+    },
+    data() {
+      return {
+        wallets: wallets
+    }
+  }
+  }
+    
+</script>
